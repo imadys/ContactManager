de9ecaa35e8e332c4231e9a3625467c1465009b1
imadys <git-commit-address>
2020-03-27 15:04:13 +0300

    First Push!

--- resources/views/layouts/contents/dashboard.blade.php
+++ resources/views/layouts/contents/dashboard.blade.php
@@ -36,13 +36,12 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('dashboard.index')}}">
         <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-laugh-wink"></i>
+          <i class="fas fa-id-card-alt"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">C Manager <sup>1</sup></div>
       </a>
-
       <!-- Divider -->
       <hr class="sidebar-divider my-0">
 
